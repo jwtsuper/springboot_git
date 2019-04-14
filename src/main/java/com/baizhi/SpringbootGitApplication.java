98043f4ee94cf074e162fc97a2f39b64f7c4cf5a
jwt <604459110@qq.com>
2019-04-14 14:16:08 +0800

    逆向与通用

--- src/main/java/com/baizhi/SpringbootGitApplication.java
+++ src/main/java/com/baizhi/SpringbootGitApplication.java
@@ -2,8 +2,10 @@ package com.baizhi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(value = "com.baizhi.dao")
 public class SpringbootGitApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/baizhi/SpringbootGitApplicationTests.java
+++ src/test/java/com/baizhi/SpringbootGitApplicationTests.java
@@ -1,16 +1,28 @@
 package com.baizhi;
 
+import com.baizhi.dao.UserMapper;
+import com.baizhi.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootGitApplicationTests {
+    @Autowired
+    /* private UserDao userDao;*/
+    private UserMapper userMapper;
 
     @Test
-    public void contextLoads() {
+    public void test1() {
+        List<User> list = userMapper.selectAll();
+        for (User user : list) {
+            System.out.println(user);
+        }
     }
 
 }
